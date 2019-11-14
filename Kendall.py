a238c5d50ffba37d928d0e3a51c62f7d419399fc
zhaoliyuan <zhaoliyuan@licaimofang.com>
2019-11-14 15:01:04 +0800

    newe

--- Kendall.py
+++ Kendall.py
@@ -22,8 +22,21 @@ def kendall (listOne, listTwo):
     length = len(setOne)
 
 #   the first way: define a function
-
-
+    def comp():
+        print('start')
+        element = [0,0]
+        result = list()
+        for i in range(int(len(setOne)-1)):
+            element[0] = setOne[i]
+            for j in range(i+1,len(setOne)):
+                element[1] = setOne[j]
+                newElement = element.copy()
+                print(element)
+                set_trace()
+                result.append(newElement)
+                print(result)
+                set_trace()
+    comp()
 #   the second way: we can use combinations in itertools module to help but in this way you have to make sure that combinations does not change sequence of origin list
     reversions = len(set(combinations(setOne, 2)).difference(set(combinations(setTwo, 2))))
     kendall = 1 - 2 * reversions / (length*(length-1))
